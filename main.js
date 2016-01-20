01b678c4b46f9aed6cc26623110f107920821a64
Arturo Raygoza <arturoraygozajr@gmail.com>
2016-01-20 12:34:41 -0800

    board is up but I need to fix the connection to firebase.

--- main.js
+++ main.js
@@ -1,28 +1,62 @@
-'use strict'
-
 var ref = new Firebase("https://artticktactoe.firebaseio.com/");
-var playersRef = ref.child('players');
-
+var playersRef= ref.child('players');
+var gameRef = ref.child('gameState');
+var myTurn = true;
+//var o = false;
+var cnt =0; 
+var player;
 $(document).ready(init);
-
 function init(){
-  $('#alpha').click(something);
-  $('#enter').click(enterName);
-  
-}
+  $('.col').on('click', markBox)
 
-function something(){
- 
-}
-
-function enterName(){
-  var name = $('input').val();
-  console.log('name:', name);
-  playersRef.once('value', function(snapshot){
-    if(!snapshot.val()){
-      playersRef.push(name).val());
-        (snapshot.val()).length===0){
-  playersRef.push(name)
-}
+  $('#submit').click(enterName);
+  //monitor turn child
+  ref.child('turn').on('value', function(snap){
+    var whoseTurn = snap.val();
+    if(!whoseTurn){
+      return; 
+    }
+    if(whoseTurn === player){
+      myTurn = true; 
+      return
+    }
   });
+}
+function enterName(){
+  console.log($('#username').val());
+  playersRef.once('value', function(snapshot){
+    console.log(snapshot.val());
+    if(!snapshot.val()){
+      playersRef.push($('#username').val());
+      player = 'player1';
+    }
+    else if(Object.keys(snapshot.val()).length===1){
+      playersRef.push($('#username').val());
+      player = 'player2';
+      startGame();
+    }else{
+      return;
+    }
+  });
+}
+function markBox(e){
+  if (!myTurn){
+    return;
+  }
+  if(myTurn){
+    if(player === "player1"){
+      $(e.target).text("X");
+    } else {
+      $(e.target).text("O");
+    }
+  }
+}
+function startGame(event){
+  //make turn child in firebase, and intialize it to player1.
+  ref.child('turn').set('player1');
+  /////////////
+  var boxGame = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  gameRef.set(boxGame);
+  
+  
 }
